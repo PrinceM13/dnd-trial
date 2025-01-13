c04cf7c0d7935fc1a361295e259976fa28fd23ac
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-01-13 18:45:17 +0700

    fix: add parent-restrict

--- src/features/sortable-list/components/TodoList/TodoList.tsx
+++ src/features/sortable-list/components/TodoList/TodoList.tsx
@@ -2,7 +2,7 @@
 
 import { DndContext } from "@dnd-kit/core";
 import { SortableContext } from "@dnd-kit/sortable";
-import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import { restrictToParentElement, restrictToVerticalAxis } from "@dnd-kit/modifiers";
 import TodoItem from "@/features/sortable-list/components/TodoItem";
 import useTodoList from "./TodoList.hook";
 
@@ -17,7 +17,10 @@ export const TodoList: React.FC = () => {
     <div className="select-none w-80 bg-neutral-800 text-neutral-200 rounded-2xl flex flex-col gap-6 p-6">
       <h3>Todo List</h3>
       <section className="flex-1 flex flex-col gap-4">
-        <DndContext modifiers={[restrictToVerticalAxis]} onDragEnd={handleDragEnd}>
+        <DndContext
+          modifiers={[restrictToVerticalAxis, restrictToParentElement]}
+          onDragEnd={handleDragEnd}
+        >
           <SortableContext items={todos}>
             {todos.map((todo) => (
               <TodoItem key={todo.id} todo={todo} />
